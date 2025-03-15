ccc04a3ec7d6cdea4fe5d5a5d43c43d5137f90c7
Kevin <KevD3V@gmail.com>
2025-03-14 22:32:54 -0400

    Functionality Update
    Clear button added to UI. Add references to HTML Elements.
    Add click Event Listeners to HTML Elements.

---
+++ scripts.js
@@ -0,0 +1,118 @@
+/**
+ * References to HTML Elements
+ */
+
+const oneBTN = document.querySelector("#one");
+const twoBTN = document.querySelector("#two");
+const threeBTN = document.querySelector("#three");
+const fourBTN = document.querySelector("#four");
+const fiveBTN = document.querySelector("#five");
+const sixBTN = document.querySelector("#six");
+const sevenBTN = document.querySelector("#seven");
+const eightBTN = document.querySelector("#eight");
+const nineBTN = document.querySelector("#nine");
+const zeroBTN = document.querySelector("#zero");
+
+const addBTN = document.querySelector("#add");
+const subtractBTN = document.querySelector("#subtract");
+const multiplyBTN = document.querySelector("#multiply");
+const divideBTN = document.querySelector("#divide");
+
+const decimalBTN = document.querySelector("#decimal");
+const clearBTN = document.querySelector("#clear");
+const equalsBTN = document.querySelector("#equals");
+
+
+const results = document.querySelector("#results");
+
+
+/**
+ * Add Event Listeners
+ */
+
+oneBTN.addEventListener("click", () => {
+    console.log("ONE!");
+
+});
+
+twoBTN.addEventListener("click", () => {
+    console.log("TWO!");
+
+});
+
+threeBTN.addEventListener("click", () => {
+    console.log("THREE!");
+
+});
+
+fourBTN.addEventListener("click", () => {
+    console.log("FOUR!");
+
+});
+
+fiveBTN.addEventListener("click", () => {
+    console.log("FIVE!");
+
+});
+
+sixBTN.addEventListener("click", () => {
+    console.log("SIX!");
+
+});
+
+sevenBTN.addEventListener("click", () => {
+    console.log("SEVEN!");
+
+});
+
+eightBTN.addEventListener("click", () => {
+    console.log("EIGHT!");
+
+});
+
+nineBTN.addEventListener("click", () => {
+    console.log("NINE!");
+
+});
+
+zeroBTN.addEventListener("click", () => {
+    console.log("ZERO!");
+
+});
+
+decimalBTN.addEventListener("click", () => {
+    console.log("POINT!");
+
+});
+
+addBTN.addEventListener("click", () => {
+    console.log("ADD!");
+
+});
+
+subtractBTN.addEventListener("click", () => {
+    console.log("SUBTRACT!");
+
+});
+
+
+multiplyBTN.addEventListener("click", () => {
+    console.log("MULTIPLY!");
+
+});
+
+divideBTN.addEventListener("click", () => {
+    console.log("DIVIDE!");
+
+});
+
+equalsBTN.addEventListener("click", () => {
+    console.log("EQUALS!");
+
+});
+
+clearBTN.addEventListener("click", () => {
+    console.log("CLEAR!");
+
+});
+
